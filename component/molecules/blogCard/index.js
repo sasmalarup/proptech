d9fcb9ef7be50ca201e959143eb59232f3744268
unknown <miraj.odex@gmail.com>
2023-06-14 09:07:39 +0530

    blog card ui change

--- component/molecules/blogCard/index.js
+++ component/molecules/blogCard/index.js
@@ -1,32 +1,35 @@
 import React from "react";
 import Image from "next/image";
-import { FcCalendar, FcNext } from 'react-icons/fc';
+import { FcCalendar } from 'react-icons/fc';
+import { HiOutlineArrowRight } from 'react-icons/hi'
 import hotel from'../../../public/images/rent.jpeg';
 import './styles.scss';
 
 function BlogCard() {
   return (
-    <div className="col-md-4 col-xs-12">
-        <div className="blogCard">
-          <div className="image">
-            <Image
-              src={hotel}
-              width='100px'
-              height='100px'
-              alt="rentImage"
-            />
+    <div className="col-md-6 col-xs-12">
+        <div className="blogCard row">
+          <div className="blogImage col-md-6 col-xs-12">
+            <div className="image">
+              <Image
+                src={hotel}
+                width='100px'
+                height='100px'
+                alt="rentImage"
+              />
+            </div>
+            <span className="feature_date">
+              <FcCalendar fontSize={15} color='grey' style={{marginRight: '4px'}} /> Sep 08, 2023
+            </span>
           </div>
-          <figcaption>
+          <figcaption className="col-md-6 col-xs-12">
               <div className="contentCAption">
-                    <span className="feature_date">
-                      <FcCalendar fontSize={15} color='grey' style={{marginRight: '4px'}} /> Sep 08, 2023
-                    </span>
                     <div className="feature-title">
-                        <h4>Blog Title</h4>
+                        <h4>Blog Title Apa 7 Keuntungan dan Kerugian Investasi</h4>
                     </div>
                     <div className="fp--excerpt">
                       <p>Special pricing for immediate move-ins! Call for details! AVA is a first. Our apartments are energized by New York City, personalized by you. Yep. Take our DIY kits and go...</p>
-                      <a className="continueBtn" href="{readmore}">Continue Read<FcNext fontSize={15} color='grey' style={{marginRight: '4px'}} /></a>
+                      <a className="continueBtn" href="{readmore}">Continue Read<HiOutlineArrowRight fontSize={15} style={{marginLeft: '4px'}} /></a>
                     </div>
               </div>
           </figcaption>
